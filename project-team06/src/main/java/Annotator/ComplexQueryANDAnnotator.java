0db8fa439d3f3c77a92f2c375fafece95f837746
chucycc <mf27262000@gmail.com>
2014-11-30 21:02:06 -0500

    Add comments

--- project-team06/src/main/java/Annotator/ComplexQueryANDAnnotator.java
+++ project-team06/src/main/java/Annotator/ComplexQueryANDAnnotator.java
@@ -9,34 +9,44 @@ import org.apache.uima.cas.FSIndex;
 import org.apache.uima.cas.FSIterator;
 import org.apache.uima.jcas.JCas;
 import org.apache.uima.jcas.cas.TOP;
+import org.apache.uima.resource.ResourceInitializationException;
 
 import util.Utils;
 import edu.cmu.lti.oaqa.type.retrieval.AtomicQueryConcept;
 import edu.cmu.lti.oaqa.type.retrieval.ComplexQueryConcept;
 import edu.cmu.lti.oaqa.type.retrieval.QueryOperator;
 
+/**
+ * ComplexQueryANDAnnotator combines AtomicQueryConcept into ComplexQueryConcept.
+ * 
+ * @author Hua Tang
+ * 
+ */
 public class ComplexQueryANDAnnotator extends JCasAnnotator_ImplBase {
 
-	@Override
-	public void process(JCas aJCas) throws AnalysisEngineProcessException {
-		// TODO Auto-generated method stub
-		ArrayList<AtomicQueryConcept> atomicList = new ArrayList<AtomicQueryConcept>();
-		FSIterator<TOP> atomicIter = aJCas.getJFSIndexRepository()
-				.getAllIndexedFS(AtomicQueryConcept.type);
-
-		// Iterator atomicIter = atomicIndex.iterator();
-
-		while (atomicIter.hasNext()) {
-			AtomicQueryConcept atomic = (AtomicQueryConcept) atomicIter.next();
-			atomicList.add(atomic);
-		}
-
-		ComplexQueryConcept complex = new ComplexQueryConcept(aJCas);
-		QueryOperator operator = new QueryOperator(aJCas);
-		operator.setName("AND");
-		complex.setOperator(operator);
-		complex.setOperatorArgs(Utils.fromCollectionToFSList(aJCas, atomicList));
-		complex.addToIndexes();
-	}
+  /**
+   * This method extracts atomic concepts from CAS and combines them into complex concepts.
+   * 
+   * @throws AnalysisEngineProcessException
+   */
+  @Override
+  public void process(JCas aJCas) throws AnalysisEngineProcessException {
+    // Retrieve AtomicQueryConcept from CAS
+    ArrayList<AtomicQueryConcept> atomicList = new ArrayList<AtomicQueryConcept>();
+    FSIterator<TOP> atomicIter = aJCas.getJFSIndexRepository().getAllIndexedFS(
+            AtomicQueryConcept.type);
+    // Add all of AtomicQueryConcepts to an ArrayList
+    while (atomicIter.hasNext()) {
+      AtomicQueryConcept atomic = (AtomicQueryConcept) atomicIter.next();
+      atomicList.add(atomic);
+    }
+    // Make a ComplexQueryConcpep from these AtomicQueryConcepts
+    ComplexQueryConcept complex = new ComplexQueryConcept(aJCas);
+    QueryOperator operator = new QueryOperator(aJCas);
+    operator.setName("AND");
+    complex.setOperator(operator);
+    complex.setOperatorArgs(Utils.fromCollectionToFSList(aJCas, atomicList));
+    complex.addToIndexes();
+  }
 
 }
